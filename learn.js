dc7e78d00bd25032222cb9283b765b12204580fe
Soorya S Das <sooryasandra6@gmail.com>
2026-06-17 23:00:18 +0530

    string

--- learn.js
+++ learn.js
@@ -1,5 +1,20 @@
+// String Methods
+let word = "arya";
+// console.log(word.length); // 4
+// console.log(word.indexOf("a")); // 0
+// console.log(word.charAt(2)); // y
+// console.log(word.charAt(0).toUpperCase()+word.slice(1)); // Arya
+// console.log(word.slice(0,2)); // ar
+// console.log(word.slice(-3, -1)); // ya
+
+
+let str = "hello";
+let reversedStr = str.split('').reverse().join('');
+console.log(reversedStr);
+
+
 // Number Methods
-const value = 2.46;
+// const value = 2.46;
 // console.log(Math.ceil(value)); // 3
 // console.log(Math.floor(value)); // 2
 // console.log(Math.round(value)); // 2
@@ -8,9 +23,9 @@ const value = 2.46;
 // console.log(value.toFixed(2)); // 2.46
 // console.log(value.toFixed(3)); // 2.460
 // console.log(typeof value.toFixed(3)); // string
-const result = value.toFixed(1);
-console.log(result);
-console.log(result + 1); // 2.51
+// const result = value.toFixed(1);
+// console.log(result);
+// console.log(result + 1); // 2.51
 
 
 
