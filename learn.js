fec7b524de14fcba12eebdec778a2624f102804f
Soorya S Das <sooryasandra6@gmail.com>
2026-06-17 21:59:24 +0530

    number methods

--- learn.js
+++ learn.js
@@ -1,20 +1,36 @@
+// Number Methods
+const value = 2.46;
+// console.log(Math.ceil(value)); // 3
+// console.log(Math.floor(value)); // 2
+// console.log(Math.round(value)); // 2
+// console.log(Math.round(value*10)/10); // 2.5
+// console.log(value.toFixed(1)); // 2.5
+// console.log(value.toFixed(2)); // 2.46
+// console.log(value.toFixed(3)); // 2.460
+// console.log(typeof value.toFixed(3)); // string
+const result = value.toFixed(1);
+console.log(result);
+console.log(result + 1); // 2.51
+
+
+
 // primitive - immutable
 
-let lang1 = "en";
-let lang2 = lang1;
-lang2 = "fr";
-console.log(lang1);
-console.log(lang2);
+// let lang1 = "en";
+// let lang2 = lang1;
+// lang2 = "fr";
+// console.log(lang1);
+// console.log(lang2);
 
 
 // non-premitive - mutable
-const name1 = {name : "soorya"};
-const name2 = name1;
-name2.name = "sandra";
+// const name1 = {name : "soorya"};
+// const name2 = name1;
+// name2.name = "sandra";
 
-if(name1 == name2){
-    console.log("true");
-}
-else {
-    console.log("false");
-}
+// if(name1 == name2){
+//     console.log("true");
+// }
+// else {
+//     console.log("false");
+// }
