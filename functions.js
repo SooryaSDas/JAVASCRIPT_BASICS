b986e949a29830424118d9a38ea05cccdee3b048
Soorya S Das <sooryasandra6@gmail.com>
2026-06-23 22:53:37 +0530

    types of function

--- functions.js
+++ functions.js
@@ -25,3 +25,19 @@
 // sum2(function (){},2)
 
 
+// function are 2 types 1) function declaration 2) function expression
+
+// function declaration
+function sum(a, b) {
+    return a + b;
+}
+
+// function expression - assign to a variable, also it will call it named function expression
+let sum2 = function(a, b) {
+    return a + b;
+};
+
+// this will called the anonymous function expression
+let calculate = function(a,b){
+    return a+b;
+}
