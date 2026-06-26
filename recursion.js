8bd889395390c3d5eb932a75ba99f63fef702696
Soorya S Das <sooryasandra6@gmail.com>
2026-06-26 21:53:19 +0530

    recursion countdown

---
+++ recursion.js
@@ -0,0 +1,12 @@
+// recursion countdown
+
+function countdown(n){
+    if(n === 0){
+        console.log("Go");
+        return;
+    }
+    console.log(n);
+    countdown(n - 1);
+}
+
+countdown(3);
