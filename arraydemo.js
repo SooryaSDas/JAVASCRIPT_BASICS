2684403f466c3cb825915ff0803785f678c7f794
Soorya S Das <sooryasandra6@gmail.com>
2026-06-28 18:24:42 +0530

    array

---
+++ arraydemo.js
@@ -0,0 +1,7 @@
+let num =[1,2,3,4,5];
+console.log(num); //[ 1, 2, 3, 4, 5 ]
+console.log(num[0]); //1
+console.log(num.length); // 5
+console.log(num[num.length-1]); //5
+console.log(num.at(-1)); //5
+console.log(num.at(-2)) //4
