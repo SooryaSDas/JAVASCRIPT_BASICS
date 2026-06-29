547ba56c221b355734c7e1f335b4be0f72dbc461
Soorya S Das <sooryasandra6@gmail.com>
2026-06-29 21:56:48 +0530

    array methods

--- recursion.js
+++ recursion.js
@@ -1,12 +1,22 @@
 // recursion countdown
 
-function countdown(n){
-    if(n === 0){
-        console.log("Go");
-        return;
-    }
-    console.log(n);
-    countdown(n - 1);
+// function countdown(n){ 
+//     if(n === 0){
+//         console.log("Go");
+//         return;
+//     }
+//     console.log(n);
+//     countdown(n - 1);
+// }
+
+// countdown(3);
+
+
+// rest operators
+
+function sum(...numbers){
+    return numbers.reduce((total,num)=>total+num);
 }
 
-countdown(3);
+console.log(sum(10,20)) //30
+console.log(sum(10,20,30,40,50)) //150
