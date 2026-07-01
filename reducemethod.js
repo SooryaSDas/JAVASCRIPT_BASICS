005c7d9fee7448e199a9c1ce3d767e4d0a4b80a4
Soorya S Das <sooryasandra6@gmail.com>
2026-07-01 22:20:17 +0530

    Update reducemethod.js

---
+++ reducemethod.js
@@ -0,0 +1,16 @@
+// Find the sum of all numbers
+const nums = [10, 20, 30, 40];
+let value = nums.reduce((acc,num)=>{
+    return acc + num;
+})
+// console.log(value) //100
+
+// Find the product of all numbers
+const nums1 = [2, 3, 4];
+let value1 = nums1.reduce((acc,nums)=>{
+    return acc * nums;
+})
+//  console.log(value1) //24
+
+// Find the largest number
+
