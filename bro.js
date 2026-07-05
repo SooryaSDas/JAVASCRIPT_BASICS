f1acf78b47d612f16056de10a56996b9f627547e
Soorya S Das <sooryasandra6@gmail.com>
2026-07-05 20:06:36 +0530

    top rated course

--- bro.js
+++ bro.js
@@ -2,7 +2,7 @@ const course = [
   {
     name: "javaScript Basics",
     price: 1000,
-    rating: 4.57,
+    rating: 2.57,
     isAvailable: false,
     languages: ["Malayalam", "Tamil"],
     mentor: { name: "subin", phone: "9898989889" },
@@ -10,7 +10,7 @@ const course = [
   {
     name: "java",
     price: 1000,
-    rating: 4.57,
+    rating: 3.57,
     isAvailable: false,
     languages: ["Malayalam", "Tamil"],
     mentor: { name: "subin", phone: "9898989889" },
@@ -26,7 +26,7 @@ const course = [
   {
     name: "Sql",
     price: 1000,
-    rating: 4.57,
+    rating: 1.57,
     isAvailable: false,
     languages: ["Malayalam", "Tamil"],
     mentor: { name: "subin", phone: "9898989889" },
@@ -39,7 +39,8 @@ const formateName = (name) => {
 
 function displayCourse() {
   const courseContainer = document.getElementById("course-container");
-  const courseUI = `<div class="course-title">
+  course.forEach((course) => {
+    const courseUI = `<div class="course-title">
    <h3>Course Name: ${formateName(course.name)}</h3>
    <p> Price : ${course.price}</p>
    <p> Rating : ${Math.round(course.rating * 10) / 10}</p>
@@ -47,6 +48,33 @@ function displayCourse() {
    <p> Languages : ${course.languages.join(", ")}</p>
    <p> Mentor : ${formateName(course.mentor.name)} (${course.mentor.phone})</p>
   </div>`;
-  courseContainer.innerHTML = courseUI;
+    courseContainer.innerHTML += courseUI;
+  });
 }
 displayCourse();
+
+document.getElementById("course-count").innerText =
+  `Total Courses: ${course.length}`;
+
+function getTopRatedCourse(course) {
+  let topRatedCourse = course.reduce((topcourse, currentcourse) => {
+    return topcourse.rating > currentcourse.rating ? topcourse : currentcourse;
+  });
+  return topRatedCourse;
+}
+
+function topratedcourse(arr) {
+  const topratedcourse = getTopRatedCourse(arr);
+  console.log(topratedcourse);
+  const courseUI = `<div class="course-title">
+   <h3>Course Name: ${formateName(topratedcourse.name)}</h3>
+   <p> Price : ${topratedcourse.price}</p>
+   <p> Rating : ${Math.round(topratedcourse.rating * 10) / 10}</p>
+   <p> Available : ${topratedcourse.isAvailable ? "Yes" : "No"}</p>
+   <p> Languages : ${topratedcourse.languages.join(", ")}</p>
+   <p> Mentor : ${formateName(topratedcourse.mentor.name)} (${topratedcourse.mentor.phone})</p>
+  </div>`;
+  const topcourse = document.getElementById("top-rated-courses");
+  topcourse.innerHTML = courseUI;
+}
+topratedcourse(course);
