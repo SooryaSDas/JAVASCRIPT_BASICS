d39c617fd112d549918b4b6d5c54cedefce7eaf4
Soorya S Das <sooryasandra6@gmail.com>
2026-07-08 20:39:55 +0530

    Update objectdemo.js

--- objectdemo.js
+++ objectdemo.js
@@ -1,6 +1,7 @@
 const obj = {
     name: "soorya",
-    age: 24
+    age: 24,
+    "favourite food" : "parotta and paneer",
 }
 // console.log(obj); { name: 'soorya', age: 24 }
 // console.log(obj.name); soorya
@@ -9,3 +10,6 @@ const key = "age";
 console.log(obj["key"]) //undefined
 console.log(obj[key]) // 24
 console.log(obj["name"]) // soorya
+console.log(obj["favourite food"]) // parotta and paneer
+delete obj["favourite food"]
+console.log(obj); // { name: 'soorya', age: 24 }
