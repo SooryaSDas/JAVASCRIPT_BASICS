a3b52917ec0e4353c365e9d0648a576d39b1cab8
Soorya S Das <sooryasandra6@gmail.com>
2026-07-08 20:37:27 +0530

    Update objectdemo.js

--- objectdemo.js
+++ objectdemo.js
@@ -5,3 +5,7 @@ const obj = {
 // console.log(obj); { name: 'soorya', age: 24 }
 // console.log(obj.name); soorya
 // console.log(obj.age);  24
+const key = "age";
+console.log(obj["key"]) //undefined
+console.log(obj[key]) // 24
+console.log(obj["name"]) // soorya
