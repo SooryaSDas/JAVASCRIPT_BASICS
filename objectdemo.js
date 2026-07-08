83c64db736c0b2011be1420e2ca481c2844b04c9
Soorya S Das <sooryasandra6@gmail.com>
2026-07-08 20:52:04 +0530

    Update objectdemo.js

--- objectdemo.js
+++ objectdemo.js
@@ -6,10 +6,17 @@ const obj = {
 // console.log(obj); { name: 'soorya', age: 24 }
 // console.log(obj.name); soorya
 // console.log(obj.age);  24
-const key = "age";
-console.log(obj["key"]) //undefined
-console.log(obj[key]) // 24
-console.log(obj["name"]) // soorya
-console.log(obj["favourite food"]) // parotta and paneer
-delete obj["favourite food"]
-console.log(obj); // { name: 'soorya', age: 24 }
+// const key = "age";
+// console.log(obj["key"]) //undefined
+// console.log(obj[key]) // 24
+// console.log(obj["name"]) // soorya
+// console.log(obj["favourite food"]) // parotta and paneer
+// delete obj["favourite food"]
+// console.log(obj); // { name: 'soorya', age: 24 }
+
+// computed property
+let a = "value";
+// let b = {a: "hello"};
+// console.log(b); // { a: 'hello' }
+// let b = {[a]: "hello"};
+console.log(b) // { value: 'hello' }
