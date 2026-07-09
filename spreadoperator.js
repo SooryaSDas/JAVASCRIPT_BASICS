d749d69bf93efa18e6b4ef6f84a18265a6f59a3d
Soorya S Das <sooryasandra6@gmail.com>
2026-07-09 21:13:47 +0530

    obj

--- spreadoperator.js
+++ spreadoperator.js
@@ -1,3 +1,9 @@
+//spread 
 const arr = [1,2,3];
 const arr2 = [4,5,6];
 console.log([...arr,...arr2]) // [ 1, 2, 3, 4, 5, 6 ]
+
+const obj = {a:10, b:20};
+const obj2 = {c:30, d:40};
+console.log({...obj, ...obj2}); // { a: 10, b: 20, c: 30, d: 40 }
+
